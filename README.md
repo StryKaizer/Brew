38abff1a8946a1769c8e0890823e63f69661acbf
Jimmy Henderickx <jimmyhdx@gmail.com>
2013-02-11 23:43:20 +0100

    Settings and readme changes

--- README.md
+++ README.md
@@ -11,6 +11,23 @@ Brew is intended to run on a Raspberry Pi, connected to an Arduino.
 
 Brew is using these technologies: Python, Django, Javascript, jQuery, Twitter Bootstrap, Vagrant, Puppet, Gunicorn, Nginx, Supervisord, MySQL
 
+
+###Installation for development
+
+Just use vagrant and virtualbox, fire vagrant up, and go!
+
 ###Installation for vanilla Raspbian (or Debian)
 
-TODO
+Install necessary packages
+
+  sudo apt-get update
+  sudo apt-get install git-core nano libmysqlclient-dev mysql-server python-setuptools python-dev
+  sudo easy_install pip
+  sudo pip install virtualenv
+
+Create database
+  mysql -u root -p
+  CREATE DATABASE brewpi;
+  GRANT ALL PRIVILEGES ON brewpi.* TO brewpi@localhost IDENTIFIED BY 'brewpi';
+
+Setup virtual environment
--- djangoproject/brew/settings.py
+++ djangoproject/brew/settings.py
@@ -15,9 +15,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'brew',                      # Or path to database file if using sqlite3.
-        'USER': 'brew',                      # Not used with sqlite3.
-        'PASSWORD': 'brew',                  # Not used with sqlite3.
+        'NAME': 'brewpi',                      # Or path to database file if using sqlite3.
+        'USER': 'brewpi',                      # Not used with sqlite3.
+        'PASSWORD': 'brewpi',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
