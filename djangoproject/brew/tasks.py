9e33f99f7ec01b2e07dbb5618bc31f46778317d7
Jimmy Henderickx <jimmyhdx@gmail.com>
2013-03-10 23:06:42 +0100

    More refactoring

--- djangoproject/brew/tasks.py
+++ djangoproject/brew/tasks.py
@@ -9,36 +9,36 @@ MASHSTEP_STATE_HEAT = 'H'
 MASHSTEP_STATE_COOL = 'C'
 MASHSTEP_STATE_STAY = 'S'
 MASHSTEP_STATE_FINISHED = 'F'
+ARDUINO_TEMPERATURE_PIN = 2
 
 @task()
 def init_mashing(batch):
     set_variable('current_mashing_action', 'go_to_mashingstep')
     set_variable('go_to_mashingstep_direction', 'tbd')
 
-    # Start up arduino connection
+    # Initialize Arduino with Nanpy
     if not settings.ARDUINO_SIMULATION:
         from nanpy import DallasTemperature # Nanpy Initializes Arduino connection, thus using conditional import
-        sensor = DallasTemperature(2)
-        addr = sensor.getAddress(2)
+        sensor = DallasTemperature(ARDUINO_TEMPERATURE_PIN)
+        addr = sensor.getAddress(ARDUINO_TEMPERATURE_PIN)
 
+    # Run Mashing proces
     while get_variable('mashing_active', 'FALSE') == 'TRUE':
         sleep(2)  # Log om de 2 seconden
 
+        # Measure data
+        measured_data = {}
         if settings.ARDUINO_SIMULATION:
-            try:
-                # Generate semi random temperature based on previous fake temp
-                previous = MashLog.objects.filter(batch=batch).latest('id')
-                temp = "%.2f" % ((random() / 10) + previous.degrees)
-            except MashLog.DoesNotExist:
-                # Start dummy temp
-                temp = 20
+            measured_data['temp'] = get_dummy_temperature(batch)
         else:
             # Get data from Arduino
             sensor.requestTemperatures()
-            temp = sensor.getTempC(addr)
+            measured_data['temp'] = sensor.getTempC(addr)
 
-        actions = get_mashing_actions(batch, temp)
+        # Define actions depending on measured data
+        actions = get_mashing_actions(batch, measured_data)
 
+        # Log everything
         MashLog.objects.create(
             batch=batch,
             degrees=temp,
@@ -46,14 +46,15 @@ def init_mashing(batch):
             active_mashing_step_state=actions['state']
         )
 
+    # End Mashing proces
     set_variable('mashing_batch_id_active', "0")
     return 'Mashing proces ended'
 
 
 # Returns a dictionary with actions and a boolean for each action whether they need to be active
-def get_mashing_actions(batch, temp):
+def get_mashing_actions(batch, measured_data):
 
-    temp = float(temp)
+    temp = float(measured_data['temp'])
     active_mashing_step = None
 
     # current mashing action is heat/cool until next step is reached
@@ -106,3 +107,14 @@ def get_mashing_actions(batch, temp):
 
     return {'state': state, 'active_mashing_step': active_mashing_step}
 
+
+# Return dummy temp for testing
+def get_dummy_temperature(batch):
+    try:
+        # Generate semi random temperature based on previous fake temp
+        previous = MashLog.objects.filter(batch=batch).latest('id')
+        temp = "%.2f" % ((random() / 10) + previous.degrees)
+    except MashLog.DoesNotExist:
+        # Start dummy temp
+        temp = 20
+    return temp
